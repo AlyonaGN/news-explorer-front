03abe718b31057544ca7752b9c2ce7e6efc959fa
Alyona Gnevusheva <tchernovaalena@gmail.com>
2021-01-02 20:17:49 +0300

    wip: изменена логика верстки хэдэра

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -6,8 +6,10 @@ function AppHeader({ onMenuClick, isMenuShown, name, onSignout, onAuth, isDarkMo
     return (
         <header className="app-header">
             <p className="app-header__logo">NewsExplorer</p>
-            <Navigation handleMenuClick={onMenuClick} isMenuOpened={isMenuShown}/>
+                <Navigation handleMenuClick={onMenuClick} isMenuOpened={isMenuShown}/>
             <button className={isMenuShown ? "app-header__close-menu-button" : "app-header__menu-button"} onClick={onMenuClick}></button>
+            <div className={isMenuShown ? "app-header__overlay app-header__overlay_visible" : "app-header__overlay"}>   
+            </div>
         </header>
     );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,18 +5,18 @@ import { ROUTES_MAP } from '../../utils/routesMap';
 
 function Navigation({ handleMenuClick, isMenuOpened, name, onSignout, onAuth, isDarkMode }) {
     return (
-        <nav className="nav__list">
-            <NavLink className="nav__list-item" to={ROUTES_MAP.MAIN} activeClassName="nav__list-item_active">Главная</NavLink>
-            {
-                name && <NavLink className="nav__list-item" to={ROUTES_MAP.SAVED_NEWS} activeClassName="nav-list__item_active">Сохранённые статьи</NavLink>  
-            }
-            {
-            name ?
-                <button className="nav__exit-button" onClick={onSignout}>{name}<img src="#" alt="иконка выхода из профиля"></img></button>
-                :
-                <button className="nav__auth-button" onClick={onAuth}>Авторизоваться</button>
-            }
-        </nav>   
+            <nav className={isMenuOpened ? "nav__list_visible" : "nav__list"}>
+                <NavLink className="nav__list-item" to={ROUTES_MAP.MAIN} activeClassName="nav__list-item_active">Главная</NavLink>
+                {
+                    name && <NavLink className="nav__list-item" to={ROUTES_MAP.SAVED_NEWS} activeClassName="nav-list__item_active">Сохранённые статьи</NavLink>  
+                }
+                {
+                name ?
+                    <button className="nav__exit-button" onClick={onSignout}>{name}<img src="#" alt="иконка выхода из профиля"></img></button>
+                    :
+                    <button className="nav__auth-button" onClick={onAuth}>Авторизоваться</button>
+                }
+            </nav> 
     );
 }
 
